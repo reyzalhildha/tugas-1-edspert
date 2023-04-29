a79aee82e68b161299ea68563d5ba39a1ecc0075
reyzalhildha <reyzal@adiwisista.com>
2023-04-29 14:17:49 +0700

    tambah kerangka product

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import './style.css';
 import hero from './img/hero.svg';
+import people from './img/people.svg';
 
 function App() {
   return (
@@ -28,6 +29,158 @@ function App() {
         </div>
       </section>
       <section className="product">
+        <div className="container-product">
+            <div className="card">
+              <div className="header">
+                <div className="image">
+                  <img src={people} alt="people"/>
+                </div>
+                <div className="text">
+                  <h4>Intensive Bootcamp</h4>
+                  <h5>Programming Laravel</h5>
+                  <h6>(Getting Started with Laravel 9)</h6>
+                </div>
+              </div>
+              <div className="content">
+                <p>Programming Laravel Getting Started with Laravel 9</p>
+                <table>
+                  <tr>
+                    <td>Batch</td>
+                    <td>September 2022 </td>
+                  </tr>
+                  <tr>
+                    <td>Mentor</td>
+                    <td>William Hartono, Farel Prayoga </td>
+                  </tr>
+                </table>
+              </div>
+            </div>
+            <div className="card">
+              <div className="header">
+                <div className="image">
+                  <img src={people} alt="people"/>
+                </div>
+                <div className="text">
+                  <h4>Intensive Bootcamp</h4>
+                  <h5>Programming Laravel</h5>
+                  <h6>(Getting Started with Laravel 9)</h6>
+                </div>
+              </div>
+              <div className="content">
+                <p>Programming Laravel Getting Started with Laravel 9</p>
+                <table>
+                  <tr>
+                    <td>Batch</td>
+                    <td>September 2022 </td>
+                  </tr>
+                  <tr>
+                    <td>Mentor</td>
+                    <td>William Hartono, Farel Prayoga </td>
+                  </tr>
+                </table>
+              </div>
+            </div>
+            <div className="card">
+              <div className="header">
+                <div className="image">
+                  <img src={people} alt="people"/>
+                </div>
+                <div className="text">
+                  <h4>Intensive Bootcamp</h4>
+                  <h5>Programming Laravel</h5>
+                  <h6>(Getting Started with Laravel 9)</h6>
+                </div>
+              </div>
+              <div className="content">
+                <p>Programming Laravel Getting Started with Laravel 9</p>
+                <table>
+                  <tr>
+                    <td>Batch</td>
+                    <td>September 2022 </td>
+                  </tr>
+                  <tr>
+                    <td>Mentor</td>
+                    <td>William Hartono, Farel Prayoga </td>
+                  </tr>
+                </table>
+              </div>
+            </div>
+            <div className="card">
+              <div className="header">
+                <div className="image">
+                  <img src={people} alt="people"/>
+                </div>
+                <div className="text">
+                  <h4>Intensive Bootcamp</h4>
+                  <h5>Programming Laravel</h5>
+                  <h6>(Getting Started with Laravel 9)</h6>
+                </div>
+              </div>
+              <div className="content">
+                <p>Programming Laravel Getting Started with Laravel 9</p>
+                <table>
+                  <tr>
+                    <td>Batch</td>
+                    <td>September 2022 </td>
+                  </tr>
+                  <tr>
+                    <td>Mentor</td>
+                    <td>William Hartono, Farel Prayoga </td>
+                  </tr>
+                </table>
+              </div>
+            </div>
+            <div className="card">
+              <div className="header">
+                <div className="image">
+                  <img src={people} alt="people"/>
+                </div>
+                <div className="text">
+                  <h4>Intensive Bootcamp</h4>
+                  <h5>Programming Laravel</h5>
+                  <h6>(Getting Started with Laravel 9)</h6>
+                </div>
+              </div>
+              <div className="content">
+                <p>Programming Laravel Getting Started with Laravel 9</p>
+                <table>
+                  <tr>
+                    <td>Batch</td>
+                    <td>September 2022 </td>
+                  </tr>
+                  <tr>
+                    <td>Mentor</td>
+                    <td>William Hartono, Farel Prayoga </td>
+                  </tr>
+                </table>
+              </div>
+            </div>
+            <div className="card">
+              <div className="header">
+                <div className="image">
+                  <img src={people} alt="people"/>
+                </div>
+                <div className="text">
+                  <h4>Intensive Bootcamp</h4>
+                  <h5>Programming Laravel</h5>
+                  <h6>(Getting Started with Laravel 9)</h6>
+                </div>
+              </div>
+              <div className="content">
+                <p>Programming Laravel Getting Started with Laravel 9</p>
+                <table>
+                  <tr>
+                    <td>Batch</td>
+                    <td>September 2022 </td>
+                  </tr>
+                  <tr>
+                    <td>Mentor</td>
+                    <td>William Hartono, Farel Prayoga </td>
+                  </tr>
+                </table>
+              </div>
+            </div>
+        </div>
        </section>
     </>
   );
